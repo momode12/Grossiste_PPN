64d3f6f9d4237a6db012961763e0c1edb6d5c123
HERITIANA Julien <heritianajulien12@gmail.com>
2026-03-07 21:48:32 +0300

    Initialize admin user on app startup
    Added call to create_admin function to initialize admin user.

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from routes._init_routes import create_routes
 from extensions import mail
 from datetime import datetime
 from flask_cors import CORS
+from create_admin import create_admin
 
 app = Flask(__name__)
 CORS(
@@ -34,6 +35,7 @@ jwt = JWTManager(app)
 # Créer toutes les tables
 with app.app_context():
     db.create_all()
+    create_admin()
 
 # Enregistrer les blueprints (routes)
 create_routes(app)
